746c0d2c3c8f81b79d72881fe21f805b5d4f4e38
matarolli <64810732+matarolli@users.noreply.github.com>
2024-10-29 10:18:51 -0300

    Some pages

--- form1.php
+++ form1.php
@@ -3,3 +3,25 @@
 include 'src/header.php';
 include 'src/nav.php';
 ?>
+
+
+    <!-- Formulário para Método POST -->
+    <section id="post-form">
+        <h2>Formulário com Método POST</h2>
+        <form action="src/form-handler.php" method="POST">
+            <label for="email">Digite seu e-mail:</label><br>
+            <input type="email" id="email" name="email" required><br><br>
+            <input type="submit" value="Enviar">
+        </form>
+    </section>
+    
+</section>
+
+
+
+</main>
+
+<?php
+// Incluindo o rodapé
+include 'src/footer.php';
+?>
---
+++ galeria.php
@@ -0,0 +1,19 @@
+<?php
+// Incluindo o cabeçalho e a navegação
+include 'src/header.php';
+include 'src/nav.php';
+?>
+
+<main>
+
+<?php
+// Incluindo a galeria na página principal
+include 'src/gallery.php';
+?>
+
+</main>
+
+<?php
+// Incluindo o rodapé
+include 'src/footer.php';
+?>
