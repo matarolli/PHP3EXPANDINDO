9bb65339d68918cc8c23da4c17fdb8b0fc9cbcd3
Matarolli <matarolli@gmail.com>
2024-10-29 10:35:47 -0300

    Index

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ include 'src/nav.php';
 ?>
 
 <main>
-
+<section id="datetime">
+    <h2>Data e Hora Atual</h2>
+    <p>Data: <?php echo date("d/m/Y"); ?></p>
+    <p>Hora: <span id="current-time"><?php echo date("H:i:s"); ?></span></p>
+</section>
 </main>
 
 <?php
