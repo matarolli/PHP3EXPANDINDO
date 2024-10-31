d9dc42eaae06e061a43d9cb9ef44cc8e45c6fd98
Matarolli <matarolli@gmail.com>
2024-10-31 09:05:46 -0300

    fuse

--- index.php
+++ index.php
@@ -5,11 +5,15 @@ include 'src/nav.php';
 ?>
 
 <main>
-<section id="datetime">
-    <h2>Data e Hora Atual</h2>
-    <p>Data: <?php echo date("d/m/Y"); ?></p>
-    <p>Hora: <span id="current-time"><?php echo date("H:i:s"); ?></span></p>
-</section>
+    <section id="datetime">
+        <?php
+date_default_timezone_set("America/Sao_Paulo"); // Define o fuso horário para São Paulo (Brasil)
+?>
+
+        <h2>Data e Hora Atual</h2>
+        <p>Data: <?php echo date("d/m/Y"); ?></p>
+        <p>Hora: <span id="current-time"><?php echo date("H:i:s"); ?></span></p>
+    </section>
 </main>
 
 <?php
