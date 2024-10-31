e851c2f453fe776d9e01a2899ba609c9f8c2eb9b
Matarolli <matarolli@gmail.com>
2024-10-31 10:00:38 -0300

    asdupdate

--- src/header.php
+++ src/header.php
@@ -11,5 +11,15 @@
 
 <body>
     <header>
-
+        <div id="text-container">
+            <div id="line-one">
+                <h1>Bem </h1< />
+            </div>
+            <div id="line-two">
+                <h1>vindo!</h1< />
+            </div>
+            <div id="line-three">
+                <h1>...</h1< />
+            </div>
+        </div>
     </header>
